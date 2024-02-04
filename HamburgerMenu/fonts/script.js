ac280aca703b9ba0e27befcd0ced1e4c37c0ecd7
dipudangol2 <dipudangol2@gmail.com>
2024-02-04 22:14:41 +0545

    Add open and close nav menu function

---
+++ HamburgerMenu/fonts/script.js
@@ -0,0 +1,30 @@
+const btnOpen = document.querySelector("#btnOpen");
+const btnClose = document.querySelector("#btnClose");
+const media = window.matchMedia("(width < 40em)");
+const topNavMenu = document.querySelector(".topnav__menu");
+function setupTopNav(e) {
+  if (e.matches) {
+    //is mobile
+    console.log("is mobile");
+    topNavMenu.setAttribute("inert", "");
+  } else {
+    //is tablet/desktop
+    console.log("is desktop");
+    topNavMenu.removeAttribute("inert");
+  }
+}
+
+function openMobileMenu() {
+  btnOpen.setAttribute("aria-expanded", "true");
+  topNavMenu.removeAttribute("inert");
+}
+function closeMobileMenu() {
+  btnOpen.setAttribute("aria-expanded", "false");
+  topNavMenu.setAttribute("inert", "");
+}
+setupTopNav(media);
+btnOpen.addEventListener("click", openMobileMenu);
+btnClose.addEventListener("click", closeMobileMenu);
+media.addEventListener("change", function (e) {
+  setupTopNav(e);
+});
